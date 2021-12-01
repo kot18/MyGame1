889b1bad6f88dd74a743a6bb978118c267386671
kot18 <95213706+kot18@users.noreply.github.com>
2021-12-01 17:28:21 +0300

    В этом файле расписано управление персонажем

---
+++ Player.py
@@ -0,0 +1,35 @@
+import pygame
+
+class player():
+
+    def __init__(self, screen):
+        "Игрок"
+
+        self.screen = screen
+        self.image = pygame.image.load('assets/player.png')
+
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+        self.center = float(self.rect.centerx)
+
+        self.mright = False
+        self.mleft = False
+
+    def visual_player(self):
+
+        self.screen.blit(self.image, self.rect)
+
+    def update_position_player(self):
+        "Обновление позиции пушки"
+
+        if self.mright == True and self.rect.right < self.screen_rect.right:
+            self.center += 1.3
+
+        if self.mleft == True and self.rect.left > 0:
+            self.center -= 1.3
+
+        self.rect.centerx = self.center
