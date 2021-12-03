d6bda3ebcd2eafe247e03cff10a6902262cb6650
kot18 <95213706+kot18@users.noreply.github.com>
2021-12-03 11:06:18 +0300

    В этом файле будет статистика игрока и его жизни

---
+++ stats.py
@@ -0,0 +1,12 @@
+class Stats():
+    """Статистика игрока"""
+
+    def __init__(self):
+        """Инициализация статистики"""
+
+        self.Reset_stats()
+
+    def Reset_stats(self):
+        """Динамическое изменеие статистики в игре"""
+
+        self.player_left = 3
