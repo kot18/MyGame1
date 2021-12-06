8c81fd625ea1378b7bbced71a75ffd90eb4d59da
kot18 <95213706+kot18@users.noreply.github.com>
2021-12-06 11:16:29 +0300

    добавлено отображение статистики игрока

--- stats.py
+++ stats.py
@@ -6,7 +6,13 @@ class Stats():
 
         self.Reset_stats()
 
+        self.run_game = True
+
+        with open('highscore.txt', 'r') as f:
+            self.high_score = int(f.readline())
+
     def Reset_stats(self):
         """Динамическое изменеие статистики в игре"""
 
         self.player_left = 3
+        self.score = 0
